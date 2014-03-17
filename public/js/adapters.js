35c93ebfd7a0f5e06e47247d5aa463c16c6c6529
Bryan Yap <bcollie115@gmail.com>
2014-03-17 01:43:20 +0000

    added default fixtures adapter

---
+++ public/js/adapters.js
@@ -0,0 +1,36 @@
+ActiveAddress.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+ActiveAddress.Contact.FIXTURES = [
+{
+  id: 4,
+  first_name: 'Affro',
+  surname: 'Kingsley',
+  address: 'Henry Adams, Rockford',
+  phone_number: '01234567890',
+  email: 'kings@rocky.org'
+},
+{
+  id: 1,
+  first_name: 'Frodo',
+  surname: 'Baggins',
+  address: 'The Burrow, Shire',
+  phone_number: '07913475574',
+  email: 'frodo@middleearth.org'
+},
+{
+  id: 2,
+  first_name: 'Harry',
+  surname: 'Potter',
+  address: 'The Cupboard under the Stairs, 4 Privet Drive, Little Whinging, Surrey',
+  phone_number: '01234567890',
+  email: 'harry.potter@hogwarts.ac.uk'
+},
+{
+  id: 3,
+  first_name: 'Benjamin',
+  surname: 'Button',
+  address: '25 Whirley Way, Hogsmead',
+  phone_number: '01429547601',
+  email: 'ben.button@buttons.com'
+}
+]
